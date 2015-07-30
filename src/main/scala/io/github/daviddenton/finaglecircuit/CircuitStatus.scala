367080db9788766164d8aa4b6e6f0736592e30ca
David Denton <denton.david@gmail.com>
2015-07-30 09:26:48 +0100

    daveD -  docs for half-open

--- src/main/scala/io/github/daviddenton/finaglecircuit/CircuitStatus.scala
+++ src/main/scala/io/github/daviddenton/finaglecircuit/CircuitStatus.scala
@@ -1,5 +1,12 @@
 package io.github.daviddenton.finaglecircuit
 
+/**
+ * Circuit states. Of note is HalfOpen:
+ * - The first call attempted is allowed through without failing fast
+ * - All other calls fail-fast with an exception just as in Open state
+ * - If the first call succeeds, the breaker is reset back to Closed state
+ * - If the first call fails, the breaker is tripped again into the Open state for another full resetTimeout
+ */
 object CircuitStatus extends Enumeration {
   type CircuitStatus = Value
   val Open, Closed, HalfOpen = Value
